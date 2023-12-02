e05aef27f1f7acb352ad85dfb3aa4976b0b5402f
Lhomas7 <landon7@clemson.edu>
2023-12-02 15:08:03 -0500

    hdjbkqanm,z

--- room_not_found_error.py
+++ room_not_found_error.py
@@ -2,4 +2,4 @@ class RoomNotFoundError(Exception):
     def __init__(self, message = 'room not found'):
         self.message = message
     def __str__(self, user_exit):
-        return f"{user_exit} -> {self.message}"
+        return f"Invalid room: {user_exit} -> {self.message}"
