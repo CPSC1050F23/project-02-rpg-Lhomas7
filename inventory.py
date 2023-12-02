172afe43eead474ce23d2982016da5de109410a7
Lhomas7 <landon7@clemson.edu>
2023-12-02 15:02:31 -0500

    hfujwkcas,

--- inventory.py
+++ inventory.py
@@ -7,7 +7,7 @@ class Inventory:
     def print_inventory(self):
         print('INVENTORY:')
         for item in self.inventory:
-            print(f'\t{item.get_item()}- {item.get_description()}')
+            print(f'\t\t{item.get_item()}- {item.get_description()}')
     def get_inventory(self):
         return self.inventory
         
